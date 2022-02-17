2153feaa7971700cad014f990a89497fa5f0217d
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-02-17 14:46:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash;dev' >>feeds.conf.default
+#echo 'src-git openclash https://github.com/vernesong/OpenClash;dev' >>feeds.conf.default
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 sed -i 's/PATCHVER:=5.10/PATCHVER:=5.15/g' target/linux/x86/Makefile
